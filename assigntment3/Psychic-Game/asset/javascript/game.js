3a03a23941a64d80db8d5cf286e72007fbb47cab
ScottTAnderson <42193132+ScottTAnderson@users.noreply.github.com>
2018-12-17 02:09:29 -0600

    assignment3

---
+++ assigntment3/Psychic-Game/asset/javascript/game.js
@@ -0,0 +1,54 @@
+//Create an array of characters from unicode letter 'charA' to letter 'charZ
+function genCharArray(charA, charZ) {
+    var arrayOfLetters = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    for (i = i; i <= j; i++) {
+        arrayOfLetters.push(String.fromCharCode(i));
+    }
+    return arrayOfLetters;
+}
+
+//Concantinate lowercase and uppercase arrays of the alphabet
+var alphabet = genCharArray("a", "z").concat(genCharArray("A", "Z"));
+
+var userLetter = document.getElementById("lettersGuessed");
+var guessesLeft = document.getElementById("guessesLeft");
+var wins = document.getElementById("wins");
+var losses = document.getElementById("losses");
+
+
+
+console.log(alphabet);
+
+
+function psychicGame() {
+    //Create a random letter for the computer which will always be upper case
+    var computerLetter = alphabet[Math.floor(Math.random() * alphabet.length)].toUpperCase();
+    //Verify the desired result
+    console.log(computerLetter);
+    document.onkeyup = function (event) {
+        var isLetter = alphabet.indexOf(event.key);
+        if (isLetter !== -1) {
+            var userLetterUpper = event.key.toUpperCase();
+            console.log(userLetterUpper);
+            console.log(guessesLeft.textContent);
+            {
+                if (userLetterUpper === computerLetter) {
+                    alert("You guess it! It was " + userLetterUpper);
+                    wins.textContent = wins.textContent + 1;
+                    userLetter.textContent = "";
+                    guessesLeft.textContent = 9;
+                    psychicGame();
+                } else if (guessesLeft.textContent > "0") {
+                    userLetter.textContent += userLetterUpper + ", ";
+                    guessesLeft.textContent -= 1;
+                } else {
+                    alert("You ran out of guesses! Play again!");
+                    losses.textContent = losses.textContent + 1;
+                    userLetter.textContent = "";
+                    guessesLeft.textContent = 9;
+                }
+            }
+        }
+    }
+};
+psychicGame();
